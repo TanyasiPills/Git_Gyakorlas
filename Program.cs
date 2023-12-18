610983c487ff2598f49301663b82797b4a109c84
Maychii <matyasg760@gmail.com>
2023-12-18 12:16:13 +0100

    funkció: lista feltöltés

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace Git_Gyakorlas;
 class Program
 {
+    public bool megal = false;
+    public int szam;
+    public List<int> szamok = new List<int>();
+
     void Main(string[] args)
     {
+        while(megal == false){
+            Console.Write("Adjon meg egy számot: ");
+            if(int.TryParse(Console.ReadLine(), out szam)){
+            if(szam == 0) megal = true;
+            szamok.Append(szam);
+            }
+            else{
+                Console.WriteLine("Hibás érték\n");
+            }
+        }
     }
 }
