cce587c5e1a4ac712f71107b7751056f17db1cbd
Maychii <matyasg760@gmail.com>
2023-12-18 12:36:03 +0100

    funkció: kiírás

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿namespace Git_Gyakorlas;
 class Program
 {
-    public bool megal = false;
-    public int szam;
-    public List<int> szamok = new List<int>();
 
-    void Main(string[] args)
+    static void Main(string[] args)
     {
+        bool megal = false;
+        int szam;
+        List<int> szamok = new List<int>();
+
         while(megal == false){
+            
+
             Console.Write("Adjon meg egy számot: ");
             if(int.TryParse(Console.ReadLine(), out szam)){
             if(szam == 0) megal = true;
-            szamok.Append(szam);
+            szamok.Add(szam);
             }
             else{
                 Console.WriteLine("Hibás érték\n");
             }
+
+            szamok.ForEach(e => {Console.Write($"{e}, ");});
+            Console.WriteLine();
         }
     }
 }
